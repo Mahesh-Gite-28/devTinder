ffb7295e9a4c4303948189983de6a0e3a4efb62f
Mahesh <maheshgite2811@gmail.com>
2026-01-03 05:38:21 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 express=require("express");
+const {auth,userauth} = require("./middlewares/auth");
 
 app=express();
 
-app.use("/user",
-       
-[
-(req,res,next)=>{
-    next();
-},
-(req,res,next)=>{
-    res.send("this is first response");
-    next();
-}],
-(req,res,next)=>{
-    console.log("i am third handler");
-    // next();
-    // res.send("i am third route handler");
-}
-
+app.use("/admin",
+    auth
 )
 
+app.get("/user",userauth,(req,res)=>{
+    res.send("user data send");
+})
+
+app.get("/admin/getAllData",(req,res)=>{
+    //authentication
+    res.send("data is  sent");
+})
+
+app.get("/admin/deleteUser",(req,res)=>
+{
+    res.send("delete a user");
+})
 
 app.listen(7777,()=>{
     console.log("server is listening on port 7777.......");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+const auth=(req,res,next)=>
+{
+    console.log("admin auth checked");
+    const token="xyz";
+    const isauth= token==="xyz";
+    if(isauth){
+        next();
+    }
+    else
+    {
+        res.status(401).send("unauthorized response");
+    }
+}
+
+
+const userauth=(req,res,next)=>
+{
+    console.log("admin auth checked");
+    const token="xyyz";
+    const isauth= token==="xyz";
+    if(isauth){
+        next();
+    }
+    else
+    {
+        res.status(401).send("unauthorized response");
+    }
+}
+
+module.exports={auth,userauth};
